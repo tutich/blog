0116bac4c9dcab6e620b4f17bb09cf0ca2043ca2
tutich <pkoinange02@gmail.com>
2024-06-19 21:17:40 +0300

    db set-up

---
+++ blog/lib/database/models/user.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model, models } from "mongoose";
+
+// export interface IUser {
+//     clerkId: string;
+//     email: string;
+//     username: string;
+//     photo: string; // URL as a string
+//     firstName: string;
+//     lastName: string;
+//     planId?: number;
+//     creditBalance?: number;
+// }
+
+
+const UserSchema = new Schema({
+    clerkId: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
+    photo: {type: URL, required: true},
+    firstName: {type: String},
+    lastName: {type: String},
+    planId: {type: Number, default: 1,},
+    creditBalance: {type: Number, default: 10}
+    
+});
+
+const User = models?.User || model('User', UserSchema);
+
+export default User;
