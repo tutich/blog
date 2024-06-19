c299c192f0ee83d89cb8a2e24a6330c186fd703a
tutich <pkoinange02@gmail.com>
2024-06-20 00:19:21 +0300

    shema-photo

--- blog/lib/database/models/user.model.ts
+++ blog/lib/database/models/user.model.ts
@@ -13,14 +13,39 @@ import { Schema, model, models } from "mongoose";
 
 
 const UserSchema = new Schema({
-    clerkId: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    username: {type: String, required: true, unique: true},
-    photo: {type: URL, required: true},
-    firstName: {type: String},
-    lastName: {type: String},
-    planId: {type: Number, default: 1,},
-    creditBalance: {type: Number, default: 10}
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      photo: {
+        type: String,
+        required: true,
+      },
+      firstName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+      },
+      planId: {
+        type: Number,
+        default: 1,
+      },
+      creditBalance: {
+        type: Number,
+        default: 10,
+      },
     
 });
 
