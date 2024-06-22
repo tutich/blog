b2e823f4c411aa4ed206cc21b0b85d29114be6bb
tutich <pkoinange02@gmail.com>
2024-06-23 00:15:28 +0300

    isPublic routes

--- blog/middleware.ts
+++ blog/middleware.ts
@@ -1,6 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe']);
+
+export default clerkMiddleware((auth, request) => {
+  console.log('Request URL:', request.url);
+  console.log('Is Public Route:', isPublicRoute(request));
+
+  if (!isPublicRoute(request)) {
+    console.log('Protecting Route:', request.url);
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
