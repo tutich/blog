4b831668a3f586c22bc750f49ebd0f5e2d10c2a8
tutich <pkoinange02@gmail.com>
2024-06-22 18:46:09 +0300

    auth-middleware

--- blog/middleware.ts
+++ blog/middleware.ts
@@ -1,7 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+const isPublicRoute = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe']);
+
+export default clerkMiddleware((auth, request) => {
+  if(!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
