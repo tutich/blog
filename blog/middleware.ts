911b43176e9c906206b1ca5743abac6ef46131e1
tutich <pkoinange02@gmail.com>
2024-06-23 01:29:34 +0300

    env

--- blog/middleware.ts
+++ blog/middleware.ts
@@ -1,12 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/(.*)', '/api/webhooks/clerk(.*)', '/api/webhooks/stripe(.*)']);
 
-export default clerkMiddleware((auth, request) => {
-  if(!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
